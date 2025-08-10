ca1722387ee900537cc00bcafbf60711794cce2d
Bharath Dundi <bharathdundi@gmail.com>
2025-08-10 18:14:53 +0530

    Adding brew formula

---
+++ goku.rb
@@ -0,0 +1,15 @@
+class Goku < Formula
+  desc "A powerful CLI tool named Goku"
+  homepage "https://github.com/DevopsGuyXD/Goku_Releases"
+  url "https://github.com/DevopsGuyXD/Goku_Releases/archive/refs/tags/v0.0.1_darwin_amd64.tar.gz"
+#   sha256 "PUT_SHA256_HASH_HERE"
+  license "MIT"
+
+  def install
+    bin.install "goku"
+  end
+
+  test do
+    assert_match "Goku version", shell_output("#{bin}/goku -v")
+  end
+end
