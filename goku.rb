dd6501f3a5fec03b947f584a695917f518713dbc
Bharath Dundi <77780574+DevopsGuyXD@users.noreply.github.com>
2025-08-11 02:05:08 +0530

    Update goku.rb

--- goku.rb
+++ goku.rb
@@ -1,7 +1,7 @@
 class Goku < Formula
   desc "A powerful CLI tool named Goku"
   homepage "https://github.com/DevopsGuyXD/Goku_Releases"
-  url "https://github.com/DevopsGuyXD/Goku_Releases/archive/refs/tags/v0.0.1_darwin_amd64.tar.gz"
+  url "https://github.com/DevopsGuyXD/Goku_Releases/archive/refs/tags/v0.0.2_darwin_amd64.tar.gz"
 #   sha256 "PUT_SHA256_HASH_HERE"
   license "MIT"
 
